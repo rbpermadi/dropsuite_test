f7998b2e88c7d5f084a7506108db3639c11e2a90
Rojali Budi Permadi <rojali717@gmail.com>
2020-08-11 21:57:48 +0700

    search and count indentical content

--- main.rb
+++ main.rb
@@ -1,14 +1,26 @@
+require "fileutils"
+
 def most_identical_content (base_path)
   file_paths = Dir[File.join(base_path, '**', '*')]
 
-  new_file_paths = []
+  file_path_counter = Hash.new
   file_paths.each do |file_path|
     if File.file? file_path
-        new_file_paths << file_path
+      new_file_path = ""
+      file_path_counter.each do |key, value|
+        if(FileUtils.compare_file(key,file_path))
+          new_file_path = key
+          break
+        end
+      end
+
+      new_file_path = file_path if new_file_path.empty?
+      file_path_counter[new_file_path] = 0 unless file_path_counter.key? new_file_path
+      file_path_counter[new_file_path] += 1
     end
   end
 
-  return new_file_paths
+  return file_path_counter
 end
 
 base_path = '/Users/rojalibudipermadi/Downloads/DropsuiteTest'
